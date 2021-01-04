b16a0850bc1eb2706818a1892042bdcce8beb2bf
endroni <rubao.rcs@gmail.com>
2021-01-04 09:54:07 -0300

    style proprio add

--- painel.php
+++ painel.php
@@ -4,7 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Layout Dashboard</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <!-- Boostrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"/>
+    
+    <!-- Fonts -->
+    <link rel="stylesheet" href="./css/fontawesome.min.css"/>
+
+    <!-- CSS Próprio -->
+    <link rel="stylesheet" href=".css/style.css"/>
 </head>
 <body>
     <section class="menu">
