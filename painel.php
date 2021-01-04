7ea9d7735d491ff6d8972b232dba7a1aa2fdfe42
endroni <rubao.rcs@gmail.com>
2021-01-04 11:49:16 -0300

    icon menu add

--- painel.php
+++ painel.php
@@ -6,21 +6,30 @@
     <title>Layout Dashboard</title>
 
     <!-- Boostrap CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"/>
-    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
     <!-- Fonts -->
-    <link rel="stylesheet" href="./css/fontawesome.min.css"/>
+    <link href="./css/all.css" rel="stylesheet">
 
     <!-- CSS Próprio -->
-    <link rel="stylesheet" href=".css/style.css"/>
+    <link rel="stylesheet" href="./css/style.css"/>
 </head>
 <body>
     <section class="menu">
         <div class="logo">
-            <img src="./imgs/logo-especializati.png" alt="Logo Curso" class="logo-painel">
+            <img src="./imgs/icone-especializati.png" alt="Logo Curso" class="logo-painel">
         </div>
 
         <div class="list-menu">
+            <ul>
+                <li>
+                    <a href="">   
+                        <i class="fas fa-home"></i>                    
+                        
+                        Home
+                    </a>
+                </li>
+            </ul>
 
         </div>
 
