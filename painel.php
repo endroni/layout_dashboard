c480764ed08289d7140034ec68b3855588711a98
endroni <rubao.rcs@gmail.com>
2021-01-05 11:54:32 -0300

    problem conteudo responsivo resolvido

--- painel.php
+++ painel.php
@@ -74,10 +74,12 @@
 
             <div class="content-din">
 
-                <?php for($i = 1; $i <= 10; $i++){ ?>                    
+                <div class="row">
+
+                    <?php for($i = 1; $i<=10; $i++){ ?>                    
                         <div class="col-md-3 col-sm-4 col-xm-12">
                             <div class="rel-dash">
-                                
+                        
                                 <i class="fas fa-home"></i>
                                 <div class="text-rel">
                                     <h2 class="result">
@@ -89,8 +91,9 @@
                                 </div>
                             </div>                    
                         </div>
-                    
-                <?php }?>
+                    <?php }?>
+
+                </div>
 
             </div>
         </div> <!-- Fim Content DS -->
