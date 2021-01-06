b7a34030b6b50eeef65dc408929bc85ce102f1f7
endroni <rubao.rcs@gmail.com>
2021-01-06 13:36:09 -0300

    create tela de login

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+
+    <!-- CSS Próprio -->
+    <link rel="stylesheet" href="./css/login-style.css"/>    
+
+    <!--Favicon-->
+    <link rel="icon" type="image/png" href="./imgs/favicon.png">
+<body class="bg-login">
+	<section class="login">
+		<div class="image-login">
+			<img src="./imgs/logo-especializati.png" alt="Especializa TI" class="img-login">
+		</div>
+
+		<form class="form-login">
+				<input type="text" name="email" placeholder="E-mail">
+				<input type="password" name="password" placeholder="Senha">
+
+				<button class="btn btn-second">Acessar</button>
+
+				<a href="" class="rel-pass">Recuperar Senha?</a>
+		</form>
+
+	</section>
+</body>
+</html>
